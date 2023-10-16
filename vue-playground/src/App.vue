46b9b5f87597933c45d5c050e1b784682ccdfc8e
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-10-16 13:24:52 +0200

    Router example

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -9,31 +9,15 @@
 
   <message-example message="new message" v-bind:show2ndMessage="true"/>-->
 
-  <!--<p>Choose what part of this page you want to see:</p>
-  <button @click="activeComp = 'animal-collection'">Animals</button>
-  <button @click="activeComp = 'food-items'">Food</button><br>
+  <p>Choose what part of this page you want to see:</p>
+  <router-link to="/animals">Animals</router-link>
+  <router-link to="/food">Food</router-link><br>
   <div>
-    <component :is="activeComp"></component>
-  </div>-->
-  <http-call/>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-//export default {
-//  methods: {
-//    receiveEmit(foodName) {
-//      alert('Hello '+ foodName);
-//    }
-//  }
-//};
-export default {
-  data() {
-      return {
-        activeComp: ''
-      }
-    }
-}
-</script>
+
 
 <!--<style>
 #app > div {
@@ -46,9 +30,17 @@ export default {
 </style>-->
 
 <style scoped>
-  button {
+  a {
+    display: inline-block;
+    background-color: black;
+    border: solid 1px black;
+    color: white;
     padding: 5px;
     margin: 10px;
+  }
+  a:hover,
+  a.router-link-active {
+    background-color: rgb(110, 79, 13);
   }
   div {
     border: dashed black 1px;
--- vue-playground/src/main.js
+++ vue-playground/src/main.js
@@ -1,29 +1,41 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+// Required for the Router
+import { createRouter, createWebHistory } from 'vue-router'
+
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import CompOne from './components/CompOne.vue'
-import HttpCallExample from'./components/HttpCallExample.vue'
-import FormExample from'./components/FormExample.vue'
-import MessageExample from'./components/MessageExample.vue'
+//import FoodItem from './components/FoodItem.vue'
+//import CompOne from './components/CompOne.vue'
+//import HttpCallExample from'./components/HttpCallExample.vue'
+//import FormExample from'./components/FormExample.vue'
+//import MessageExample from'./components/MessageExample.vue'
 
 // Routing Example Components
 import FoodItems from'./routingexample/FoodItems.vue'
 import AnimalCollection from'./routingexample/AnimalCollection.vue'
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/animals', component: AnimalCollection },
+        { path: '/food', component: FoodItems },
+    ]
+});
+// End Routing Example
+
 const app = createApp(App)
-app.component('food-item', FoodItem);
-app.component('teleport-example', CompOne);
-app.component('http-call', HttpCallExample);
-app.component('form-example', FormExample);
-app.component('message-example', MessageExample);
-
-// Routing example
-app.component('food-items', FoodItems);
-app.component('animal-collection', AnimalCollection);
-
-
+app.use(router);
 app.mount('#app')
+
+//app.component('food-item', FoodItem);
+//app.component('teleport-example', CompOne);
+//app.component('http-call', HttpCallExample);
+//app.component('form-example', FormExample);
+//app.component('message-example', MessageExample);
+
+
+
 
 
